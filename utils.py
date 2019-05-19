32407321fc3678afa42083f853b27b8607810455
mrsalehi <salehi.mohammadreza1997@gmail.com>
2019-05-19 17:36:22 +0430

    main.py modified. utils.py added

---
+++ utils.py
@@ -0,0 +1,15 @@
+
+def read_rules(path):
+    path = 'LL1.txt'
+    with open(path, 'r') as f:
+        last_nt = None
+        rules = dict()
+        for line in f:
+            new_nt, rule = line.split('->')
+            if new_nt != last_nt:
+                last_nt = new_nt
+                rules[new_nt] = [rule.split()]
+            else:
+                rules[last_nt].append(rule.split())
+
+    return rules
