e0dddc5370b7d405ef027e5f42a303aa3910d66d
mrsalehi <salehi.mohammadreza1997@gmail.com>
2019-05-19 16:59:49 +0430

    main modified

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 import lexer
+import parser
 
 
-lexer = lexer.Lexer("input_code2.c")
+def func_phase1(lexer):
+    with open('scanner.txt', 'w') as fout, open('errors_lexical.txt', 'w') as ferr:
+        last_line_out = 0
+        last_line_err = 0
+        while True:
+            tp = lexer.get_next_token()
+            print(tp[1])
+            new_line = tp[1][0]
 
-with open('scanner.txt', 'w') as fout, open('errors_lexical.txt', 'w') as ferr:
-    last_line_out = 0
-    last_line_err = 0
-    while True:
-        tp = lexer.get_next_token()
-        print(tp[1])
-        new_line = tp[1][0]
+            if tp[1][2] != 'invalid input':
+                if new_line > last_line_out:
+                    if last_line_out > 0:
+                        fout.write('\n')
+                    fout.write(str(new_line) + '. ')
+                    last_line_out = new_line
+                fout.write('(' + tp[1][1] + ', ' + tp[1][2] + ')' + ' ')
+            else:
+                if new_line > last_line_err:
+                    if last_line_err > 0:
+                        ferr.write('\n')
+                    ferr.write(str(new_line) + '. ')
+                    last_line_err = new_line
+                ferr.write('(' + tp[1][1] + ', ' + tp[1][2] + ')' + ' ')
 
-        if tp[1][2] != 'invalid input':
-            if new_line > last_line_out:
-                if last_line_out > 0:
-                    fout.write('\n')
-                fout.write(str(new_line) + '. ')
-                last_line_out = new_line
-            fout.write('(' + tp[1][1] + ', ' + tp[1][2] + ')' + ' ')
-        else:
-            if new_line > last_line_err:
-                if last_line_err > 0:
-                    ferr.write('\n')
-                ferr.write(str(new_line) + '. ')
-                last_line_err = new_line
-            ferr.write('(' + tp[1][1] + ', ' + tp[1][2] + ')' + ' ')
+            if not tp[0]:
+                break
 
-        if not tp[0]:
-            break
+
+if __name__ == '__main__':
+    lexer = lexer.Lexer("input_code2.c")
+
+    terminals = []
+
+    parser = parser.Parser(lexer=lexer, rules=None, first_sets=None, follow_sets=None,
+                           non_terminals=None, terminals=None)
